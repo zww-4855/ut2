1a83d69c94aaf29a4cb9a4c553bf9cca2c88c564
Zachary Windom <zww4855@gmail.com>
2023-03-03 16:45:59 -0500

    Corrected testing for Ne in 3 basis sets -- size consistency

---
+++ UT2/tests/test_T2module_size_consistency.py
@@ -0,0 +1,52 @@
+import pytest
+
+import UT2
+
+import numpy as np
+import pyscf
+from pyscf import ao2mo
+import itertools
+from UT2.run_ccd import * 
+
+def test_Size_consistencyRHFT2():
+    # run pyscf for some reason
+    basis = ['ccpvdz','6-31G','ccpvtz']#,'augccpvdz','augccdpvtz']
+    atomString = ['Ne 0 0 0; Ne 0 0 50', 'Ne 0 0 0']
+    testMethods=[{"ccdType":"CCD"}]
+    value=[]
+    count=0
+    for bas, molecule,method in itertools.product(basis, atomString,testMethods):
+        mol = pyscf.M(
+            atom=molecule,
+            verbose=5,
+            basis=bas)
+
+
+
+        mf = mol.RHF()
+        mf.conv_tol_grad=1E-10
+        mf.run()
+
+
+        orb=mf.mo_coeff
+        cc_runtype=method   #{"ccdType":"CCDQf-1"}
+
+        correlatedEnergy,corrCo=ccd_main(mf,mol,orb,cc_runtype)
+        value.append(correlatedEnergy)
+        print('Final energy:', correlatedEnergy)
+        if count%2 == 1:
+            diff=abs(abs(value[0])-abs(value[1]*2))
+            assert diff <= 10**-10
+            value=[]
+        count+=1
+        print('Final energy:', correlatedEnergy)
+
+#    diff=value[0]-value[1]*2
+#    assert diff <= 10**-10
+
+def main():
+    basis = ['ccpvdz']#,'ccpvtz','augccpvdz','augccdpvtz']
+    atomString = ['Ne 0 0 0; Ne 0 0 50', 'Ne 0 0 0']
+    testMethods=[{"ccdType":"CCD"}]
+
+test_Size_consistencyRHFT2()
--- UT2/tests/test_UT2.py
+++ UT2/tests/test_UT2.py
@@ -20,35 +20,3 @@ def test_UT2_imported():
     assert "UT2" in sys.modules
     assert "pyscf" in sys.modules
 
-def test_T2module_size_consistency():
-    # run pyscf for some reason
-    basis = ['ccpvdz']#,'ccpvtz','augccpvdz','augccdpvtz']
-    atomString = ['Ne 0 0 0; Ne 0 0 50', 'Ne 0 0 0']
-    testMethods=[{"ccdType":"CCD"}]
-    value=[]
-    for bas, molecule,method in itertools.product(basis, atomString,testMethods):
-        mol = pyscf.M(
-            atom=molecule,
-            verbose=5,
-            basis=bas)
-    
-    
-    
-        mf = mol.RHF()
-        mf.conv_tol_grad=1E-10
-        mf.run()
-    
-    
-    
-        cc_runtype=method   #{"ccdType":"CCDQf-1"}
-    
-        correlatedEnergy=run_ccd.ccd_main(mf,mol,orb,cc_runtype)
-        value.append(correlatedEnergy)
-
-        print('Final energy:', correlatedEnergy)
-
-    diff=value[0]-value[1]*2
-    assert diff <= 10**-10
-
-
-
