fb90d86c43a31adc507a529a0f0a520cbffcdd70
Zachary Windom <zww4855@gmail.com>
2023-03-03 22:28:37 -0500

    Added test to verify RHF/UHF based CC should give equivalent answers before bifurcation of F2

---
+++ UT2/tests/test_RHF_UHF_energyequiv.py
@@ -0,0 +1,56 @@
+import pytest
+
+import UT2
+
+import numpy as np
+import pyscf
+from pyscf import ao2mo
+import itertools
+from UT2.run_ccd import * 
+
+def test_Size_consistencyRHFT2():
+    # run pyscf for some reason
+    basis = ['ccpvdz']#,'6-31G','ccpvtz']#,'augccpvdz','augccdpvtz']
+    atomString = ['F 0 0 0; F 0 0 1.1']
+    testMethods=[{"ccdType":"CCD"}] #,{"ccdType":"CCDQf-1"},{"ccdType":"CCDQf-2"},{"ccdType":"CCD(Qf)"},{"ccdType":"pCCD"}]
+    value=[]
+    count=0
+    for bas, molecule,method in itertools.product(basis, atomString,testMethods):
+        mol = pyscf.M(
+            atom=molecule,
+            verbose=5,
+            basis=bas)
+
+
+        for i in range(2):
+            if i==0:
+                mf = mol.RHF()
+            else:
+                mf = mol.UHF()
+
+            mf.conv_tol_grad=1E-10
+            mf.run()
+    
+    
+            orb=mf.mo_coeff
+            cc_runtype=method   #{"ccdType":"CCDQf-1"}
+    
+            correlatedEnergy,corrCo=ccd_main(mf,mol,orb,cc_runtype)
+            value.append(correlatedEnergy)
+            print('Final energy:', correlatedEnergy)
+            if count%2 == 1:
+                diff=abs(abs(value[0])-abs(value[1]))
+                assert diff <= 10**-10
+                value=[]
+            count+=1
+            print('Final energy:', correlatedEnergy)
+
+#    diff=value[0]-value[1]*2
+#    assert diff <= 10**-10
+
+def main():
+    basis = ['ccpvdz']#,'ccpvtz','augccpvdz','augccdpvtz']
+    atomString = ['Ne 0 0 0; Ne 0 0 50', 'Ne 0 0 0']
+    testMethods=[{"ccdType":"CCD"}]
+
+test_Size_consistencyRHFT2()
