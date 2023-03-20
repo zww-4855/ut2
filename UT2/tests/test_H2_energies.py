6ee8f8a275a351ff4eee298dd7b279d851bf932c
Zachary Windom <zww4855@gmail.com>
2023-03-20 15:29:58 -0400

    streamlined testing

--- UT2/tests/test_H2_energies.py
+++ UT2/tests/test_H2_energies.py
@@ -8,36 +8,45 @@ from pyscf import ao2mo
 import itertools
 from UT2.run_ccd import * 
 
-def test_H2energy():
-    # run pyscf for some reason
-    basis = ['ccpvdz']#,'6-31G','ccpvtz']#,'augccpvdz','augccdpvtz']
-    atomString = ['H 0 0 0; H 0 0 0.98']
-    testMethods=[{"ccdType":"CCD"},{"ccdType":"CCDQf-1"},{"ccdType":"CCDQf-2"},{"ccdType":"CCD(Qf)"}]
-    value=[]
-    count=0
-    for bas, molecule,method in itertools.product(basis, atomString,testMethods):
-        mol = pyscf.M(
-            atom=molecule,
-            verbose=5,
-            basis=bas)
+''' Test that ensures all higher order T2 methods beyond CCD reduces to CCD whenever    terms associated with T4, T6, etc are 0 by construction. In this case, the test     involves H2, where we know effects associated with T4, etc are zero because 
+    there are only 2 electrons. CCD reference energy taken from ACES2 result. '''
 
+@pytest.mark.parametrize("Basis,Method",[('ccpvdz',{"ccdType":"CCD"}), 
+    ('ccpvtz',{"ccdType":"CCD"}),
+    ('augccpvtz',{"ccdType":"CCD"}),
+    ('ccpvdz',{"ccdType":"CCDQf-1"}),
+    ('ccpvtz',{"ccdType":"CCDQf-1"}),
+    ('augccpvtz',{"ccdType":"CCDQf-1"}),
+    ('ccpvdz',{"ccdType":"CCDQf-2"}),
+    ('ccpvtz',{"ccdType":"CCDQf-2"}),
+    ('augccpvtz',{"ccdType":"CCDQf-2"}),
+    ('ccpvdz',{"ccdType":"CCD(Qf)"}),
+    ('ccpvtz',{"ccdType":"CCD(Qf)"}),
+    ('augccpvtz',{"ccdType":"CCD(Qf)"}),])
 
-        mf = mol.RHF()
+def test_H2energy(Basis,Method):
+    if Basis=="ccpvdz":
+        aces2CCD=-1.142679147402
+    elif Basis=="ccpvtz":
+        aces2CCD=-1.148497898757
+    elif Basis=="augccpvtz":
+        aces2CCD=-1.149022864393
+    atomString = 'H 0 0 0; H 0 0 1.851932'
+    mol = pyscf.M(
+        atom=atomString,
+        verbose=5,
+        unit='B',
+        basis=Basis)
+    mf = mol.RHF()
+    mf.conv_tol_grad=1E-10
+    mf.run()
+    orb=mf.mo_coeff
+    cc_runtype=Method   
+    correlatedEnergy,corrCo=ccd_main(mf,mol,orb,cc_runtype)
+    diff=abs(abs(aces2CCD)-abs(correlatedEnergy))
+    assert diff <= 10**-10
+    print('Final energy:', correlatedEnergy)
 
-        mf.conv_tol_grad=1E-10
-        mf.run()
-    
-    
-        orb=mf.mo_coeff
-        cc_runtype=method   #{"ccdType":"CCDQf-1"}
-  
-        correlatedEnergy,corrCo=ccd_main(mf,mol,orb,cc_runtype)
-        if count == 0:
-            value.append(correlatedEnergy)
-        else:
-            diff=abs(abs(value[0])-abs(correlatedEnergy))
-            assert diff <= 10**-10
-        count+=1
-        print('Final energy:', correlatedEnergy)
-
-test_H2energy()
+#Basis='ccpvdz'
+#Method={"ccdType":"CCDQf-1"}
+#test_H2energy(Basis,Method)
--- UT2/tests/test_T2module_size_consistency.py
+++ UT2/tests/test_T2module_size_consistency.py
@@ -7,46 +7,47 @@ import pyscf
 from pyscf import ao2mo
 import itertools
 from UT2.run_ccd import * 
+'''
+   Test each T2 method's size-consistency property for dimeric Neon
+'''
+@pytest.mark.parametrize("Basis,Method",[('ccpvdz',{"ccdType":"CCD"}),
+    ('ccpvtz',{"ccdType":"CCD"}),
+    ('ccpvdz',{"ccdType":"CCDQf-1"}),
+    ('ccpvtz',{"ccdType":"CCDQf-1"}),
+    ('ccpvdz',{"ccdType":"CCDQf-2"}),
+    ('ccpvtz',{"ccdType":"CCDQf-2"}),
+    ('ccpvdz',{"ccdType":"CCD(Qf)"}),
+    ('ccpvtz',{"ccdType":"CCD(Qf)"}),
+    ('ccpvdz',{"ccdType":"pCCD"}),
+    ('ccpvtz',{"ccdType":"pCCD"}),])
 
-def test_Size_consistencyRHFT2():
-    # run pyscf for some reason
-    basis = ['ccpvdz']#,'6-31G','ccpvtz']#,'augccpvdz','augccdpvtz']
+
+def test_Size_consistencyRHFT2(Basis,Method):
     atomString = ['Ne 0 0 0; Ne 0 0 50', 'Ne 0 0 0']
-    testMethods=[{"ccdType":"CCD"},{"ccdType":"CCDQf-1"},{"ccdType":"CCDQf-2"},{"ccdType":"CCD(Qf)"},{"ccdType":"pCCD"}]
     value=[]
-    count=0
-    for bas, molecule,method in itertools.product(basis, atomString,testMethods):
+    for i in range(2):
         mol = pyscf.M(
-            atom=molecule,
+            atom=atomString[i],
             verbose=5,
-            basis=bas)
-
-
+            basis=Basis)
 
         mf = mol.RHF()
         mf.conv_tol_grad=1E-10
         mf.run()
 
-
         orb=mf.mo_coeff
-        cc_runtype=method   #{"ccdType":"CCDQf-1"}
+        cc_runtype=Method
 
         correlatedEnergy,corrCo=ccd_main(mf,mol,orb,cc_runtype)
         value.append(correlatedEnergy)
         print('Final energy:', correlatedEnergy)
-        if count%2 == 1:
+        if i%2 == 1:
             diff=abs(abs(value[0])-abs(value[1]*2))
             assert diff <= 10**-10
             value=[]
-        count+=1
         print('Final energy:', correlatedEnergy)
 
-#    diff=value[0]-value[1]*2
-#    assert diff <= 10**-10
 
-def main():
-    basis = ['ccpvdz']#,'ccpvtz','augccpvdz','augccdpvtz']
-    atomString = ['Ne 0 0 0; Ne 0 0 50', 'Ne 0 0 0']
-    testMethods=[{"ccdType":"CCD"}]
-
-test_Size_consistencyRHFT2()
+#Basis='ccpvdz'
+#Method={"ccdType":"CCD(Qf)"}
+#test_Size_consistencyRHFT2(Basis,Method)
