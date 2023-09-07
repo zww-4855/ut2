30359547b238caf5d8ce55e3d44e80777a9d544e
Zachary Windom <zww4855@gmail.com>
2023-09-07 12:09:31 -0400

    Added logic to load pickled T1/T2 amps and extract (T)-like energy correction; make the interface more robust git add amphandler.py ! !

--- UT2/amphandler.py
+++ UT2/amphandler.py
@@ -1,26 +1,58 @@
 import pickle
+import UT2.pdagq_T3corr as pdagq_T3corr
+import numpy as np
 
 class AmpHandler():
-    def __init__(self,storedInfo,T2infile,T1infile=None,T3infile=None):
+    def __init__(self,o,v,storedInfo,T2infile,T1infile=None,T3infile=None):
         self.t1=None
         self.t2=None
-        self.t3=None
+        self.t3={}
+        self.T2infile=T2infile
+        self.T1infile=T1infile
+        self.T3infile=T3infile
+        self.o=o
+        self.v=v
+        self.nocc=storedInfo.occInfo["nocc_aa"]
+        self.nvirt=storedInfo.occInfo["nvirt_aa"]
         self.getAmps()
         self.g=storedInfo.integralInfo["tei"]
-        self.denoms=storedInfo.denoms
+        self.fock=storedInfo.integralInfo["oei"]
+        self.denoms=storedInfo.denomInfo
+        print(self.denoms.keys())
 
 
 
     def getAmps(self):
-        with open(T2infile,'rb') as t2handle:
+        with open(self.T2infile,'rb') as t2handle:
             self.t2=pickle.load(t2handle)
 
-        if T1infile:
-            with open(T1infile,'rb') as t1handle:
+        if self.T1infile:
+            with open(self.T1infile,'rb') as t1handle:
                 self.t1=pickle.load(t1handle)
+        else: # assumes t1ordering (virt,occ)
+            self.t1=np.zeros((self.nvirt,self.nocc))
 
-        if T3infile:
-            with open(t3infile,'rb') as t3handle:
-                self.t3=pickle.load(t3handle)
 
-        
+    def run_pdagq(self):
+        # need t2 shape (virt,virt,occ,occ), so if not in this shape
+        # transpose the t2 tensor
+        if np.shape(self.t2)[0] != self.nvirt:
+            print('inside first if caution')
+            l1=self.t1
+            l2=self.t2
+            self.t2=self.t2.transpose(2,3,0,1)
+            self.t1=self.t1.transpose(1,0)
+        else:
+            print('inside second if: true')
+            print(np.shape(self.t1))
+            l1=self.t1.transpose(1,0)
+            l2=self.t2.transpose(2,3,0,1)
+        t3resid=pdagq_T3corr.build_T3(self.g,self.o,self.v,self.t2)
+        t3=t3resid*self.denoms["D3aa"]
+        #self.t3.update["T3_secondOrder"]=t3
+        #l1=self.t1 #.transpose(1,0)
+        #l2=self.t2 #.transpose(2,3,0,1)
+        energy=pdagq_T3corr.pdagq_T3energy(self.g,self.o,self.v,l1,l2,t3)
+        print('(T)-based triples correction to CCSD is:', energy)
+        pdagq_T3corr.ccsd_energy(self.g,self.fock,self.o,self.v,self.t1,self.t2)
+
